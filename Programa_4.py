4ef63fbc133d3361be2b34b9958a682d8a9caa6c
AlanAlvarado76 <luigialv76@gmail.com>
2024-05-20 11:38:54 -0600

    Progarama 5 - Mayor de edad

---
+++ Programa_4.py
@@ -0,0 +1,10 @@
+#Formula general en python
+
+a =float (2)
+b = float (4)
+c = float(-6)
+
+x1 = (-b + (b**2-(4*a*c))**0.5)/(2*a)
+x2 = (-b - (b**2-(4*a*c))**0.5)/(2*a)
+print  (x1)
+print (x2)
---
+++ Programa_5.py
@@ -0,0 +1,6 @@
+
+edad = int(input("Ingrese su edad"))
+if edad >= 18:
+    print ("tu puedes entrar al antro")
+else: 
+    print ("tu no puedes entrar al antro")
