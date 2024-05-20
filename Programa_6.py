42d0d9e188faeeab1b13b4efc4e92c71f745adb0
AlanAlvarado76 <luigialv76@gmail.com>
2024-05-20 11:59:28 -0600

    Programa 6 - Numero mayor

---
+++ Programa_6.py
@@ -0,0 +1,7 @@
+x = float(input("Ingrese un numero: "))
+y = float(input("Ingrese un segundo numero: "))
+
+if x >= y: 
+    print("El numero es mayor o igual al segundo ingresado", x, y)
+elif y > x:
+    print("El numero es mayor o igual al primer ingresado", x, y)
