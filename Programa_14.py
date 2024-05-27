8674e9faef958b18bf9c1f83d857a825e93e4e2d
AlanAlvarado76 <luigialv76@gmail.com>
2024-05-26 19:00:59 -0600

    Programa 14 - for ocon operacion

---
+++ Programa_14.py
@@ -0,0 +1,14 @@
+
+    
+lista_de_numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    
+    
+suma_total = 0
+    
+    
+for numero in lista_de_numeros:
+        suma_total += numero
+print(f"La suma de todos los elementos de la lista es: {suma_total}")
+
+
+    
