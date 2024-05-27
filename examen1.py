50f7fa448363535037f60c0a62a6bdc36fe8f3cd
AlanAlvarado76 <luigialv76@gmail.com>
2024-05-27 10:54:39 -0600

    Examen Parcial 1

---
+++ examen1.py
@@ -0,0 +1,8 @@
+numero = int(input("Introduce un numero positivo o negativo "))
+
+if numero > 0:
+    print("el numero ingresado es positivo")
+elif numero < 0:
+    print ("El numero ingresado es negativo")
+else:
+    print("el numero ingresado es igual a 0")
