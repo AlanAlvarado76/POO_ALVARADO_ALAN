ba7644ff7ad0e9e2c01080b067686aaf575ae2fa
AlanAlvarado76 <luigialv76@gmail.com>
2024-05-26 18:25:44 -0600

    Programa 9 - Contraseña

---
+++ Programa_9.py
@@ -0,0 +1,13 @@
+
+
+while True:
+    contraseña = str(input("Ingrese una contraseña: "))
+    if contraseña == "POO123":
+        print("Bienvenido al programa")
+        break
+    else:
+        print("Contraseña incorrecta")
+        
+
+
+
