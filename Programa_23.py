4220bd5e4025b5fb1ed192f8e1229f6fa77fb594
AlanAlvarado76 <luigialv76@gmail.com>
2024-06-21 07:26:09 -0600

    Programa 23 - Clases y Objetos

---
+++ Programa_23.py
@@ -0,0 +1,22 @@
+class Carro:
+    def __init__(self, marca, modelo, kilometraje):
+        self.marca = marca
+        self.modelo = modelo
+        self.kilometraje = kilometraje
+
+        def andar(self):
+            print("Ruunn runnnn")  #Metodos
+
+        def reabastecerse(self):
+            print("reabasteciendo")  #Metodos
+
+
+
+
+carro1 = Carro("Ford","Mustang",25000)
+carro2 = Carro("Porsche","Gt3 RS", 30000)
+carro3 = Carro("Chevrolet","Corvette",78000)
+
+print(carro1)
+print(carro2)
+
